5c055d05cecfc01f37f52d284aac7634ae879462
Aarush-wadhia <aarushwadhia@gmail.com>
2024-09-30 07:45:46 +0530

    fix(xcframework,ios): local xcframework move to github and moved to github. implemented iOS ola maps

---
+++ OlaMapViewManager.m
@@ -0,0 +1,8 @@
+//
+//  OlaMapViewManager.m
+//  react-native-ola-maps
+//
+//  Created by Yash Wadhia on 27/09/24.
+//
+
+#import <Foundation/Foundation.h>
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
           altitude: 5,
           zoomLevel: 10,
         }}
+        tileUrl="https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json?key=0.4.0"
       />
     </View>
   );
